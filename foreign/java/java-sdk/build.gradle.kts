86d38f13576ebfc04f874dd095af5735038d018e
Maciej Modzelewski <maciej@modzelewski.dev>
2025-08-03 18:42:22 +0200

    chore(java): update Java SDK version (#2072)

--- foreign/java/java-sdk/build.gradle.kts
+++ foreign/java/java-sdk/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 }
 
 group = "org.apache.iggy"
-version = "0.2.0-SNAPSHOT"
+version = "0.5.0"
 
 repositories {
     mavenCentral()
@@ -69,7 +69,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.apache.iggy"
             artifactId = "iggy-java-sdk"
-            version = "0.2.0-SNAPSHOT"
+            version = "0.5.0"
 
             from(components["java"])
 
