a4e4fd23eb44ae22cee1ce65b891c05352c36fcd
Nandakumar Vadivelu <nanda@apache.org>
2025-08-13 17:10:04 +0530

    chore(sdk): update Java SDK version

--- foreign/java/java-sdk/build.gradle.kts
+++ foreign/java/java-sdk/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 }
 
 group = "org.apache.iggy"
-version = "0.5.0"
+version = "0.5.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -74,7 +74,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "org.apache.iggy"
             artifactId = "iggy-java-sdk"
-            version = "0.5.0"
+            version = "0.5.0-SNAPSHOT"
 
             from(components["java"])
 
