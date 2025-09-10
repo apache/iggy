7d583c15f758fa493f4c5108e201f66ba524f5ce
Edgar Modesto <edgarmodesto23@gmail.com>
2025-09-10 14:47:51 -0700

    Refactor iceberg sink

--- core/connectors/sinks/iceberg_sink/src/lib.rs
+++ core/connectors/sinks/iceberg_sink/src/lib.rs
@@ -16,6 +16,7 @@
  * under the License.
  */
 
+use core::fmt;
 use std::collections::HashMap;
 use std::io::Cursor;
 use std::sync::Arc;
@@ -24,6 +25,7 @@ use arrow_json::ReaderBuilder;
 use async_trait::async_trait;
 
 use iceberg::arrow::schema_to_arrow_schema;
+use iceberg::table::Table;
 use iceberg::transaction::{ApplyTransactionAction, Transaction};
 use iceberg::writer::base_writer::data_file_writer::DataFileWriterBuilder;
 use iceberg::writer::file_writer::ParquetWriterBuilder;
@@ -38,11 +40,7 @@ use iggy_connector_sdk::{
     sink_connector, ConsumedMessage, Error, MessagesMetadata, Payload, Sink, TopicMetadata,
 };
 use parquet::file::properties::WriterProperties;
-use s3::bucket::Bucket;
-use s3::creds::Credentials;
-use s3::region::Region;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use tracing::{error, info};
 use uuid::Uuid;
 
@@ -50,7 +48,19 @@ use uuid::Uuid;
 #[allow(non_camel_case_types)]
 pub enum IcebergSinkTypes {
     rest,
-    hdfs,
+    hive,
+    glue,
+}
+
+impl fmt::Display for IcebergSinkTypes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            IcebergSinkTypes::rest => "rest",
+            IcebergSinkTypes::hive => "hive",
+            IcebergSinkTypes::glue => "glue",
+        };
+        write!(f, "{}", s)
+    }
 }
 
 sink_connector!(IcebergSink);
@@ -59,7 +69,8 @@ sink_connector!(IcebergSink);
 pub struct IcebergSink {
     id: u32,
     config: IcebergSinkConfig,
-    client: reqwest::Client,
+    tables: Vec<Table>,
+    catalog: Option<RestCatalog>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -79,121 +90,18 @@ pub struct IcebergSinkConfig {
 }
 
 impl IcebergSink {
-    fn flatten_payload(json: &Value) -> Vec<(String, Option<String>)> {
-        let payload = json.get("payload").and_then(|p| p.get("Json"));
-        vec![
-            (
-                "id".to_string(),
-                payload
-                    .and_then(|p| p.get("id"))
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string()),
-            ),
-            (
-                "title".to_string(),
-                payload
-                    .and_then(|p| p.get("title"))
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string()),
-            ),
-            (
-                "name".to_string(),
-                payload
-                    .and_then(|p| p.get("name"))
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string()),
-            ),
-            (
-                "text".to_string(),
-                payload
-                    .and_then(|p| p.get("text"))
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string()),
-            ),
-            (
-                "test_field".to_string(),
-                payload
-                    .and_then(|p| p.get("test_field"))
-                    .and_then(|v| v.as_str())
-                    .map(|s| s.to_string()),
-            ),
-            ("message".to_string(), Some("".to_string())), // fill missing field
-        ]
-    }
     pub fn new(id: u32, config: IcebergSinkConfig) -> Self {
+        let tables: Vec<Table> = Vec::with_capacity(config.tables.len());
         IcebergSink {
             id,
             config,
-            client: reqwest::Client::new(),
+            tables,
+            catalog: None,
         }
     }
-    fn get_s3_client(&self) -> Bucket {
-        let credentials = Credentials::new(
-            Some(&self.config.store_access_key_id.clone()),
-            Some(&self.config.store_secret_access_key.clone()),
-            None,
-            None,
-            None,
-        )
-        .expect("Invalid credentials");
 
-        let region = Region::Custom {
-            region: self.config.store_region.clone(),
-            endpoint: self.config.store_url.clone(),
-        };
-
-        info!("{} {:?} {:?}", self.config.bucket_name, region, credentials);
-
-        let bucket = Bucket::new(&self.config.bucket_name, region, credentials)
-            .expect("Failed to create S3 bucket client");
-
-        *bucket.with_path_style()
-    }
-
-    fn get_iceberg_tables(&self) -> Vec<Vec<String>> {
-        let tables: Vec<Vec<String>> = self
-            .config
-            .tables
-            .iter()
-            .map(|table| {
-                table
-                    .split('.')
-                    .map(|element| element.to_string())
-                    .collect::<Vec<String>>()
-            })
-            .collect();
-
-        info!("{:?}", tables);
-        return tables;
-    }
-
-    pub fn get_iceberg_url(&self) -> String {
-        let table_path = self.get_iceberg_tables()[0].clone();
-        if table_path.is_empty() {
-            panic!("table_path must contain at least the table name");
-        }
-
-        let table_name = table_path.last().unwrap();
-        info!("{}", table_name);
-        let namespaces = &table_path[..table_path.len() - 1];
-        let namespace_path = namespaces.join(".");
-
-        info!(
-            "{}",
-            format!(
-                "{}/v1/namespaces/{}/tables/{}",
-                self.config.uri, namespace_path, table_name
-            )
-        );
-
-        if namespace_path.is_empty() {
-            format!("{}/v1/tables/{}", self.config.uri, table_name)
-        } else {
-            format!(
-                "{}/v1/namespaces/{}/tables/{}",
-                self.config.uri, namespace_path, table_name
-            )
-        }
+    fn slice_user_table(&self, table: &String) -> Vec<String> {
+        table.split('.').map(|s| s.to_string()).collect()
     }
 }
 
@@ -204,50 +112,14 @@ impl Sink for IcebergSink {
             "Opened Iceberg sink connector with ID: {} for URL: {}",
             self.id, self.config.uri
         );
-        Ok(())
-    }
 
-    async fn consume(
-        &self,
-        _topic_metadata: &TopicMetadata,
-        messages_metadata: MessagesMetadata,
-        messages: Vec<ConsumedMessage>,
-    ) -> Result<(), Error> {
         info!(
-            "Iceberg sink with ID: {} received: {} messages, format: {}",
-            self.id,
-            messages.len(),
-            messages_metadata.schema
+            "Configuring Iceberg catalog with the following config:\n-region: {}\n-url: {}\n-store class: {}\n-catalog type: {}\n",
+            self.config.store_region,
+            self.config.store_url,
+            self.config.store_class,
+            self.config.catalog_type
         );
-
-        info!("{:?}", messages[0].payload);
-        //
-        //writer.close().map_err(|error| {
-        //    error!(
-        //        "Failed to send HTTP request to ingest messages for index: {}. {error}",
-        //        self.id
-        //    );
-        //    Error::HttpRequestFailed(error.to_string())
-        //})?;
-        //
-        //let bucket = self.get_s3_client();
-        //
-        //info!("{}", bucket.url());
-        //
-        //let key = format!("nyc/users/data/{}.parquet", Uuid::new_v4());
-        //let response_bucket = bucket.put_object(&key, &buffer).await.map_err(|error| {
-        //    error!(
-        //        "Failed to commit new parquet file to Iceberg table: {}",
-        //        error
-        //    );
-        //    Error::HttpRequestFailed(error.to_string())
-        //})?;
-        //
-        //let parquet_path = format!("s3://{}/{}", self.config.bucket_name, &key);
-
-        //info!("Parquet file uploaded: {}", parquet_path);
-
-        let iceberg_url = self.get_iceberg_url();
 
         let mut props: HashMap<String, String> = HashMap::new();
 
@@ -268,87 +140,111 @@ impl Sink for IcebergSink {
             .warehouse(self.config.bucket_name.clone())
             .build();
 
-        let catalog = RestCatalog::new(catalog_config);
+        let catalog = match self.config.catalog_type {
+            IcebergSinkTypes::rest => RestCatalog::new(catalog_config),
+            IcebergSinkTypes::hive => RestCatalog::new(catalog_config),
+            IcebergSinkTypes::glue => RestCatalog::new(catalog_config),
+        };
 
-        let table = catalog
-            .load_table(&TableIdent::from_strs(["nyc", "users"]).map_err(|err| {
-                error!("Failed to load table from catalog: {}", err);
-                Error::HttpRequestFailed(err.to_string())
-            })?)
-            .await
-            .map_err(|err| {
-                error!("Failed to get table from catalog: {}", err);
+        for declared_table in &self.config.tables {
+            let sliced_table = self.slice_user_table(&declared_table);
+            let table = catalog
+                .load_table(&TableIdent::from_strs(sliced_table).map_err(|err| {
+                    error!("Failed to load table from catalog: {}", err);
+                    Error::HttpRequestFailed(err.to_string())
+                })?)
+                .await
+                .map_err(|err| {
+                    error!("Failed to get table from catalog: {}", err);
+                    Error::HttpRequestFailed(err.to_string())
+                })?;
+            self.tables.push(table);
+        }
+
+        self.catalog = Some(catalog);
+        Ok(())
+    }
+
+    async fn consume(
+        &self,
+        _topic_metadata: &TopicMetadata,
+        messages_metadata: MessagesMetadata,
+        messages: Vec<ConsumedMessage>,
+    ) -> Result<(), Error> {
+        info!(
+            "Iceberg sink with ID: {} received: {} messages, format: {}",
+            self.id,
+            messages.len(),
+            messages_metadata.schema
+        );
+        for table in &self.tables {
+            let location = DefaultLocationGenerator::new(table.metadata().clone()).unwrap();
+
+            let file_name_gen = DefaultFileNameGenerator::new(
+                Uuid::new_v4().to_string(),
+                None,
+                iceberg::spec::DataFileFormat::Parquet,
+            );
+
+            let parquet_writer_builder = ParquetWriterBuilder::new(
+                WriterProperties::default(),
+                table.metadata().current_schema().clone(),
+                table.file_io().clone(),
+                location,
+                file_name_gen,
+            );
+            let data_file_writer_builder = DataFileWriterBuilder::new(
+                parquet_writer_builder,
+                None,
+                table.metadata().default_partition_spec_id(),
+            );
+
+            let mut writer = data_file_writer_builder.build().await.map_err(|err| {
+                error!("Error while constructing data file writer: {}", err);
                 Error::HttpRequestFailed(err.to_string())
             })?;
 
-        let location = DefaultLocationGenerator::new(table.metadata().clone()).unwrap();
+            let json_messages = messages
+                .iter()
+                .filter_map(|record| match &record.payload {
+                    Payload::Json(record) => simd_json::to_string(&record).ok(),
+                    _ => panic!("aaa"),
+                })
+                .collect::<Vec<_>>()
+                .join("\n");
 
-        let file_name_gen = DefaultFileNameGenerator::new(
-            "testing".to_string(),
-            Some(Uuid::new_v4().to_string()),
-            iceberg::spec::DataFileFormat::Parquet,
-        );
+            let cursor = Cursor::new(json_messages);
 
-        let parquet_writer_builder = ParquetWriterBuilder::new(
-            WriterProperties::default(),
-            table.metadata().current_schema().clone(),
-            table.file_io().clone(),
-            location,
-            file_name_gen,
-        );
-        let data_file_writer_builder = DataFileWriterBuilder::new(
-            parquet_writer_builder,
-            None,
-            table.metadata().default_partition_spec_id(),
-        );
+            let mut reader = ReaderBuilder::new(Arc::new(
+                schema_to_arrow_schema(&table.metadata().current_schema().clone()).unwrap(),
+            ))
+            .build(cursor)
+            .unwrap();
 
-        let mut writer = data_file_writer_builder.build().await.map_err(|err| {
-            error!("Error while constructing data file writer: {}", err);
-            Error::HttpRequestFailed(err.to_string())
-        })?;
+            while let Some(batch) = reader.next() {
+                let batch_data = batch.unwrap();
+                writer.write(batch_data).await.unwrap();
+            }
 
-        let json_messages = messages
-            .iter()
-            .filter_map(|record| match &record.payload {
-                Payload::Json(record) => simd_json::to_string(&record).ok(),
-                _ => panic!("aaa"),
-            })
-            .collect::<Vec<_>>()
-            .join("\n");
+            let data_files = writer.close().await.unwrap();
 
-        let cursor = Cursor::new(json_messages);
+            let table_commit = Transaction::new(&table);
 
-        let mut reader = ReaderBuilder::new(Arc::new(
-            schema_to_arrow_schema(&table.metadata().current_schema().clone()).unwrap(),
-        ))
-        .build(cursor)
-        .unwrap();
+            let action = table_commit.fast_append().add_data_files(data_files);
 
-        while let Some(batch) = reader.next() {
-            let batch_data = batch.unwrap();
-            writer.write(batch_data).await.unwrap();
+            let tx = action.apply(table_commit).map_err(|err| {
+                error!("Failed to apply transaction: {}", err);
+                Error::HttpRequestFailed(err.to_string())
+            })?;
+
+            let _table = tx
+                .commit(self.catalog.as_ref().unwrap())
+                .await
+                .map_err(|err| {
+                    error!("Failed to apply transaction on table: {}", err);
+                    Error::HttpRequestFailed(err.to_string())
+                })?;
         }
-
-        let data_files = writer.close().await.unwrap();
-
-        let table_commit = Transaction::new(&table);
-
-        let action = table_commit.fast_append().add_data_files(data_files);
-
-        let tx = action.apply(table_commit).map_err(|err| {
-            error!("Failed to apply transaction: {}", err);
-            Error::HttpRequestFailed(err.to_string())
-        })?;
-
-        let table = tx.commit(&catalog).await.map_err(|err| {
-            error!("Failed to apply transaction on table: {}", err);
-            Error::HttpRequestFailed(err.to_string())
-        })?;
-
-        match self.config.catalog_type {
-            IcebergSinkTypes::rest => info!("Rest"),
-            IcebergSinkTypes::hdfs => info!("HDFS"),
-        };
 
         info!("Finished successfully");
 
