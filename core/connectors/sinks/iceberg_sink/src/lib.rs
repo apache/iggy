ef0d5d14c755b75ac222ab8b795a38e6213908a2
Edgar Modesto <edgarmodesto23@gmail.com>
2025-09-15 12:41:22 -0700

    Clean error handling

--- core/connectors/sinks/iceberg_sink/src/lib.rs
+++ core/connectors/sinks/iceberg_sink/src/lib.rs
@@ -24,22 +24,24 @@ use std::sync::Arc;
 use arrow_json::ReaderBuilder;
 use async_trait::async_trait;
 
+use iceberg::TableIdent;
 use iceberg::arrow::schema_to_arrow_schema;
+use iceberg::spec::Struct;
 use iceberg::table::Table;
 use iceberg::transaction::{ApplyTransactionAction, Transaction};
 use iceberg::writer::base_writer::data_file_writer::DataFileWriterBuilder;
 use iceberg::writer::file_writer::ParquetWriterBuilder;
 use iceberg::writer::{IcebergWriter, IcebergWriterBuilder};
-use iceberg::TableIdent;
 use iceberg::{
-    writer::file_writer::location_generator::{DefaultFileNameGenerator, DefaultLocationGenerator},
     Catalog,
+    writer::file_writer::location_generator::{DefaultFileNameGenerator, DefaultLocationGenerator},
 };
 use iceberg_catalog_glue::{GlueCatalog, GlueCatalogConfig};
 use iceberg_catalog_rest::{RestCatalog, RestCatalogConfig};
 use iggy_connector_sdk::{
-    sink_connector, ConsumedMessage, Error, MessagesMetadata, Payload, Sink, TopicMetadata,
+    ConsumedMessage, Error, MessagesMetadata, Payload, Sink, TopicMetadata, sink_connector,
 };
+use parquet::data_type::DataType;
 use parquet::file::properties::WriterProperties;
 use serde::{Deserialize, Serialize};
 use tracing::{error, info};
@@ -49,15 +51,22 @@ use uuid::Uuid;
 #[allow(non_camel_case_types)]
 pub enum IcebergSinkTypes {
     rest,
-    hive,
     glue,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[allow(non_camel_case_types)]
+pub enum IcebergSinkStoreClass {
+    s3,
+    gcs,
+    fs,
+    azdls,
 }
 
 impl fmt::Display for IcebergSinkTypes {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let s = match self {
             IcebergSinkTypes::rest => "rest",
-            IcebergSinkTypes::hive => "hive",
             IcebergSinkTypes::glue => "glue",
         };
         write!(f, "{}", s)
@@ -151,8 +160,8 @@ impl IcebergSink {
             .build();
 
         let catalog = GlueCatalog::new(config).await.map_err(|err| {
-            error!("Failed to apply transaction on table: {}", err);
-            Error::HttpRequestFailed(err.to_string())
+            error!("Failed to get glue catalog with error: {}. Make sure the catalog is correctly declared on the config file", err);
+            Error::InitError(err.to_string())
         });
         return catalog;
     }
@@ -176,7 +185,6 @@ impl Sink for IcebergSink {
 
         let catalog: Box<dyn Catalog> = match self.config.catalog_type {
             IcebergSinkTypes::rest => Box::new(self.get_rest_catalog()),
-            IcebergSinkTypes::hive => Box::new(self.get_rest_catalog()),
             IcebergSinkTypes::glue => Box::new(self.get_glue_catalog().await?),
         };
 
@@ -184,13 +192,13 @@ impl Sink for IcebergSink {
             let sliced_table = self.slice_user_table(&declared_table);
             let table = catalog
                 .load_table(&TableIdent::from_strs(sliced_table).map_err(|err| {
-                    error!("Failed to load table from catalog: {}", err);
-                    Error::HttpRequestFailed(err.to_string())
+                    error!("Failed to load table from catalog: {}. Is the table {} a valid Iceberg table?", err, declared_table);
+                    Error::InitError(err.to_string())
                 })?)
                 .await
                 .map_err(|err| {
-                    error!("Failed to get table from catalog: {}", err);
-                    Error::HttpRequestFailed(err.to_string())
+                    error!("Failed to load table from catalog: {}. Is the table {} a valid Iceberg table?", err, declared_table);
+                    Error::InitError(err.to_string()) 
                 })?;
             self.tables.push(table);
         }
@@ -213,7 +221,15 @@ impl Sink for IcebergSink {
         );
 
         for table in &self.tables {
-            let location = DefaultLocationGenerator::new(table.metadata().clone()).unwrap();
+            let location =
+                DefaultLocationGenerator::new(table.metadata().clone()).map_err(|err| {
+                    error!(
+                        "Failed to get location on table: {}. Error: {}",
+                        table.metadata().uuid(),
+                        err
+                    );
+                    Error::InvalidConfig
+                })?;
 
             let file_name_gen = DefaultFileNameGenerator::new(
                 Uuid::new_v4().to_string(),
@@ -228,6 +244,7 @@ impl Sink for IcebergSink {
                 location,
                 file_name_gen,
             );
+
             let data_file_writer_builder = DataFileWriterBuilder::new(
                 parquet_writer_builder,
                 None,
@@ -236,14 +253,14 @@ impl Sink for IcebergSink {
 
             let mut writer = data_file_writer_builder.build().await.map_err(|err| {
                 error!("Error while constructing data file writer: {}", err);
-                Error::HttpRequestFailed(err.to_string())
+                Error::InitError(err.to_string())
             })?;
 
             let json_messages = messages
                 .iter()
                 .filter_map(|record| match &record.payload {
                     Payload::Json(record) => simd_json::to_string(&record).ok(),
-                    _ => panic!("aaa"),
+                    _ => None,
                 })
                 .collect::<Vec<_>>()
                 .join("\n");
@@ -251,33 +268,65 @@ impl Sink for IcebergSink {
             let cursor = Cursor::new(json_messages);
 
             let mut reader = ReaderBuilder::new(Arc::new(
-                schema_to_arrow_schema(&table.metadata().current_schema().clone()).unwrap(),
+                schema_to_arrow_schema(&table.metadata().current_schema().clone()).map_err(
+                    |err| {
+                        error!(
+                            "Error while mapping records to Iceberg table with uuid: {}. Error {}",
+                            table.metadata().uuid(),
+                            err
+                        );
+                        Error::InvalidRecord
+                    },
+                )?,
             ))
             .build(cursor)
-            .unwrap();
+            .map_err(|err| {
+                error!(
+                    "Error while building Iceberg reader from message payload: {}",
+                    err
+                );
+                Error::InitError(err.to_string())
+            })?;
 
             while let Some(batch) = reader.next() {
-                let batch_data = batch.unwrap();
-                writer.write(batch_data).await.unwrap();
+                let batch_data = batch.map_err(|err| {
+                    error!("Error while getting record batch: {}", err);
+                    Error::InvalidRecord
+                })?;
+                writer.write(batch_data).await.map_err(|err| {
+                    error!("Error while writing record batch: {}", err);
+                    Error::InvalidRecord
+                })?;
             }
 
-            let data_files = writer.close().await.unwrap();
+            let data_files = writer.close().await.map_err(|err| {
+                error!("Error while writing data records to Parquet file: {}", err);
+                Error::InvalidRecord
+            })?;
 
             let table_commit = Transaction::new(&table);
 
             let action = table_commit.fast_append().add_data_files(data_files);
 
             let tx = action.apply(table_commit).map_err(|err| {
-                error!("Failed to apply transaction: {}", err);
-                Error::HttpRequestFailed(err.to_string())
+                error!(
+                    "Failed to apply transaction on table with UUID: {}, Error: {}",
+                    table.metadata().uuid(),
+                    err
+                );
+                Error::InvalidRecord
             })?;
 
             let _table = tx
                 .commit(self.catalog.as_ref().unwrap().as_ref())
                 .await
                 .map_err(|err| {
-                    error!("Failed to apply transaction on table: {}", err);
-                    Error::HttpRequestFailed(err.to_string())
+                    error!(
+                        "Failed to commit transaction on table with UUID: {}, Error: {}",
+                        table.metadata().uuid(),
+                        err
+                    );
+                    Error::InvalidRecord
                 })?;
         }
 
