3760c9b803f864d010dbf4d01be503ddea6402e0
shin <sars21@hanmail.net>
2026-01-11 00:26:17 +0900

    test(server): refactor message retrieval tests to use real Iggy API
    - Add get_messages_by_offset_api.rs for offset-based polling tests
    - Add get_messages_by_timestamp_api.rs for timestamp-based polling tests
    - Add message_retrieval.rs with test_matrix (72 test combinations)
    - Remove consumer_timestamp_polling_scenario.rs (replaced by new tests)
    - Cover 432 parameter combinations per polling strategy via server reuse
    
    Closes #1657
    
    Signed-off-by: seokjin0414 <sars21@hanmail.net>
    Signed-off-by: shin <sars21@hanmail.net>

--- core/integration/tests/server/general.rs
+++ core/integration/tests/server/general.rs
@@ -16,9 +16,9 @@
 // under the License.
 
 use crate::server::{
-    ScenarioFn, authentication_scenario, bench_scenario, consumer_timestamp_polling_scenario,
-    create_message_payload_scenario, message_headers_scenario, run_scenario,
-    stream_size_validation_scenario, system_scenario, user_scenario,
+    ScenarioFn, authentication_scenario, bench_scenario, create_message_payload_scenario,
+    message_headers_scenario, run_scenario, stream_size_validation_scenario, system_scenario,
+    user_scenario,
 };
 use iggy_common::TransportProtocol;
 use serial_test::parallel;
@@ -34,7 +34,6 @@ use test_case::test_matrix;
         create_message_payload_scenario(),
         stream_size_validation_scenario(),
         bench_scenario(),
-        consumer_timestamp_polling_scenario(),
     ]
 )]
 #[tokio::test]
