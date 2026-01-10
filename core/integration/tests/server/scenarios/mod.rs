65a984be511de03a6d0d1527082a21a79160bbef
Chenrui Wu <chenrui@svecco.org>
2026-01-10 23:36:34 +0800

    feat(server): implement log rotation based on size and retention
    - implemented log rotation based on size and retention as the title;
    - implemented configurable attributions and imported breaking changes;
    - added units and integration test in logger.rs and intergration mod;
    - added documentations and imported new dependencies, etc.
    
    Details can be looked up at Apache Iggy #2452

--- core/integration/tests/server/scenarios/mod.rs
+++ core/integration/tests/server/scenarios/mod.rs
@@ -28,6 +28,7 @@ pub mod consumer_timestamp_polling_scenario;
 pub mod create_message_payload;
 pub mod delete_segments_scenario;
 pub mod encryption_scenario;
+pub mod log_rotation_scenario;
 pub mod message_headers_scenario;
 pub mod message_size_scenario;
 pub mod read_during_persistence_scenario;
