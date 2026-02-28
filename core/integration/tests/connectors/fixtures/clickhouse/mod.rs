c5d19e3870ef2af37e5d7939dec2fa2506c46847
kriti-sc <kathuriakriti1@gmail.com>
2026-02-28 21:18:59 +0530

    add clickhouse sink e2e tests

--- core/integration/tests/connectors/fixtures/clickhouse/mod.rs
+++ core/integration/tests/connectors/fixtures/clickhouse/mod.rs
@@ -17,22 +17,7 @@
  * under the License.
  */
 
-mod clickhouse;
-mod elasticsearch;
-mod iceberg;
-mod postgres;
-mod quickwit;
-mod wiremock;
+mod container;
+mod sink;
 
-pub use clickhouse::{
-    ClickHouseSinkFixture, ClickHouseSinkRowBinaryFixture, ClickHouseSinkStringFixture,
-};
-pub use elasticsearch::{ElasticsearchSinkFixture, ElasticsearchSourcePreCreatedFixture};
-pub use iceberg::{DEFAULT_NAMESPACE, DEFAULT_TABLE, IcebergOps, IcebergPreCreatedFixture};
-pub use postgres::{
-    PostgresOps, PostgresSinkByteaFixture, PostgresSinkFixture, PostgresSinkJsonFixture,
-    PostgresSourceByteaFixture, PostgresSourceDeleteFixture, PostgresSourceJsonFixture,
-    PostgresSourceJsonbFixture, PostgresSourceMarkFixture, PostgresSourceOps,
-};
-pub use quickwit::{QuickwitFixture, QuickwitOps, QuickwitPreCreatedFixture};
-pub use wiremock::{WireMockDirectFixture, WireMockWrappedFixture};
+pub use sink::{ClickHouseSinkFixture, ClickHouseSinkRowBinaryFixture, ClickHouseSinkStringFixture};
