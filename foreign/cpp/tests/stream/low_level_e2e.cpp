5486409ae07bf5641482bb28c7e4c6789be37bde
Rimuksh Kansal <nihonrk@gmail.com>
2026-03-12 01:54:16 +0900

    partially address comments

--- foreign/cpp/tests/stream/low_level_e2e.cpp
+++ foreign/cpp/tests/stream/low_level_e2e.cpp
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// TODO(slbotbm): create fixture for setup/teardown.
+
 #include <cstdint>
 #include <string>
 
--- foreign/cpp/tests/topic/low_level_e2e.cpp
+++ foreign/cpp/tests/topic/low_level_e2e.cpp
@@ -15,6 +15,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
+// TODO(slbotbm): create fixture for setup/teardown.x
+
 #include <cstdint>
 #include <string>
 #include <unordered_map>
