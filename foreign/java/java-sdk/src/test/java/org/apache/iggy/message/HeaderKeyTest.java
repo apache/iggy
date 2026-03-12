93b13d60639251d4a9f11eac78fb30150e3a44a6
Jonathon Henderson <jrhenderson1988@hotmail.com>
2026-03-12 17:58:34 +0000

    correct import path for assertJ assertions

--- foreign/java/java-sdk/src/test/java/org/apache/iggy/message/HeaderKeyTest.java
+++ foreign/java/java-sdk/src/test/java/org/apache/iggy/message/HeaderKeyTest.java
@@ -24,8 +24,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class HeaderKeyTest {
 
