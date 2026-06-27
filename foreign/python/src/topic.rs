93042e364ab068ae1befaf074c751fa259ebfd74
Matthew Patton <matthewpatton@macbookpro.mynetworksettings.com>
2026-06-27 07:20:05 -0400

    refactor(python): merge Topic and TopicDetails imports into one use

--- foreign/python/src/topic.rs
+++ foreign/python/src/topic.rs
@@ -15,8 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use iggy::prelude::Topic as RustTopic;
-use iggy::prelude::TopicDetails as RustTopicDetails;
+use iggy::prelude::{Topic as RustTopic, TopicDetails as RustTopicDetails};
 use pyo3::prelude::*;
 use pyo3_stub_gen::derive::{gen_stub_pyclass, gen_stub_pymethods};
 
