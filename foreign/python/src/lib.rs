305d15f3cf47adc411346a4cb1e655b63eb3c6fa
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2026-07-19 05:00:52 +0530

    feat(python): add stream listing, update, delete, and purge
    Signed-off-by: Faisal Ahmed <faisalahmedfarooq46@gmail.com>

--- foreign/python/src/lib.rs
+++ foreign/python/src/lib.rs
@@ -31,7 +31,7 @@ use consumer::{
 use pyo3::prelude::*;
 use receive_message::{PollingStrategy, ReceiveMessage};
 use send_message::SendMessage;
-use stream::StreamDetails;
+use stream::{Stream, StreamDetails};
 use topic::{Topic, TopicDetails};
 
 /// A Python module implemented in Rust.
@@ -41,6 +41,7 @@ fn apache_iggy(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<ReceiveMessage>()?;
     m.add_class::<IggyClient>()?;
     m.add_class::<StreamDetails>()?;
+    m.add_class::<Stream>()?;
     m.add_class::<Topic>()?;
     m.add_class::<TopicDetails>()?;
     m.add_class::<ConsumerGroup>()?;
