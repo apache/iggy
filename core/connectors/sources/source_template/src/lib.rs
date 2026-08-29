1d5d2809e438a8bd95f341d849b630760cdfe257
ryerraguntla <raveendra.yerraguntla@gmail.com>
2026-08-29 13:30:31 +0530

    Removing trailing space

--- core/connectors/sources/source_template/src/lib.rs
+++ core/connectors/sources/source_template/src/lib.rs
@@ -45,7 +45,7 @@
 
 use async_trait::async_trait;
 use iggy_connector_sdk::retry::{
-    CircuitBreaker, ConnectivityConfig, build_retry_client, 
+    CircuitBreaker, ConnectivityConfig, build_retry_client,
     check_connectivity_with_retry, parse_duration,
 };
 use iggy_connector_sdk::{
